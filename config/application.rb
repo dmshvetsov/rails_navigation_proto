5cfb0044aea2f5e5017aa54b4e538a49e38e0b08
shvetsovdm <sorrows@mail.ru>
2015-03-29 19:06:05 +1000

    CHANGE app name

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FerorNavigationProto
+module RailsNavigationProto
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
 
