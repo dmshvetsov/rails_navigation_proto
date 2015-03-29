537e3fe339620992eb1d1f5ac2c568669805ad0d
shvetsovdm <sorrows@mail.ru>
2015-03-29 10:21:38 +1000

    ADD SectionBase class with tree/list func.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326130747) do
+ActiveRecord::Schema.define(version: 20150329001531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "section_hierarchies", id: false, force: :cascade do |t|
+    t.integer "ancestor_id",   null: false
+    t.integer "descendant_id", null: false
+    t.integer "generations",   null: false
+  end
+
+  add_index "section_hierarchies", ["ancestor_id", "descendant_id", "generations"], name: "section_anc_desc_idx", unique: true, using: :btree
+  add_index "section_hierarchies", ["descendant_id"], name: "section_desc_idx", using: :btree
 
   create_table "sections", force: :cascade do |t|
     t.integer  "parent_id"
--- lib/section_base.rb
+++ lib/section_base.rb
@@ -1,3 +1,20 @@
+##
+# Experiment base class for navigation
+#
+# = SectionBase
+#
+# SectionBase it's abstract model to build concrete models to store pages structure of an app.
+# Sections can be becomes tree.
+# Sections can be ordered.
+#
 class SectionBase < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.inherited(subclass)
+    super
+    subclass.class_exec do
+      has_closure_tree
+      acts_as_list scope: :parent
+    end
+  end
 end
