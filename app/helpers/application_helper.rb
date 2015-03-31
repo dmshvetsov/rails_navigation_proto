12791bff1fc3ccf2c7bf334741e56526172699e4
shvetsovdm <sorrows@mail.ru>
2015-03-31 22:45:59 +1000

    ADD menu tree

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # block that renders dynamic navigation
+  def main_menu(tree = nil)
+    tree = Section.find_by(slug: 'en').try(:hash_tree) unless tree
+    if tree
+      proc do |menu|
+        tree.each do |section, childs| 
+          menu.item :section, section.title, section_url(section), {}, &main_menu(childs) if childs
+        end
+      end
+    end
+  end
 end
