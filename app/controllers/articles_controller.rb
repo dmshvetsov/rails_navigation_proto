90acb90cf679c3de31bbff58e831a7ed760f418f
shvetsovdm <sorrows@mail.ru>
2015-04-01 22:35:39 +1000

    ADD create article in section

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :slug, :body, :position)
+      params.require(:article).permit(:title, :slug, :body, :position, :section_id)
     end
 end
