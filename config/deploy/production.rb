2921e922e2e5b26f1f79b8c7de99f45fdb662485
shvetsovdm <sorrows@mail.ru>
2015-04-05 15:06:39 +1000

    CHG deploy vars

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '128.199.167.14', user: 'root', roles: %w{app db web}
+server '128.199.167.14', user: 'rails', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
