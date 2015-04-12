8b6c24a865f33e6a304d3f2359f51396a5693ded
shvetsovdm <sorrows@mail.ru>
2015-04-12 14:19:50 +1000

    ADD simple admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :sections
-  resources :pages
+  scope :admin do
+    resources :sections
+    resources :pages
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
